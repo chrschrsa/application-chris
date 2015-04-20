efb584231dbc5885da1fd48e9a17916cb8c65da4
christopher.sherlock <chrssherlock@gmail.com>
2015-04-20 13:01:36 -0400

    Chris init commit.

---
+++ app-service/src/main/java/com/test/app_service/App.java
@@ -0,0 +1,29 @@
+package com.test.app_service;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext ctx = SpringApplication.run(App.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
